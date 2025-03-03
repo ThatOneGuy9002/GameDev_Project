a2657e4d4c804d6d46458d851b71a3d8744496b4
ThatOneGuy9002 <connortfreeman@outlook.com>
2025-03-03 22:20:50 +0000

    Added basic Weapon system
    player can now fire projectiles from their character

---
+++ Assets/Scripts/WeaponSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public Transform projectileSpawn;
+
+    public float fireRate = 3f;
+    public float projectileSpeed = 3f;
+    private float timeStamp = 0f;
+
+
+    private void FixedUpdate()
+    {
+        if ((Time.time >= timeStamp) && (Input.GetKey(KeyCode.Mouse0))) 
+        {
+            Fire();
+            timeStamp = Time.time + (1/fireRate);
+        }
+    }
+
+    void Fire()
+    {
+        var projectile = (GameObject)Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation);
+
+        projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * projectileSpeed;
+
+        Destroy(projectile, 2.0f);
+    }
+
+}
